ffd4bfe711072915a18637b25ea4bc3363634f5e
dademu <A00278997@mycambrian.ca>
2023-10-12 11:54:43 -0400

    fun CalcDisplay

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.multicalculator.android"
-    compileSdk = 33
+    compileSdk = 34
     defaultConfig {
         applicationId = "com.example.multicalculator.android"
         minSdk = 24
